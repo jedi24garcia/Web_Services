00cf8c541b4c43ea3c76d5f346e324ee57750586
jedi24garcia <jedi24garcia@gmail.com>
2024-06-18 22:10:42 +1200

    added a function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Welcome to Web Services")
+
+function myFullName(name) {
+  return ("Hello, my name is " + name + "." + " " + "I live in Wellington, New Zealand.");
+}
+
+let nameVal = "Jed Garcia";
+
+console.log(myFullName(nameVal));
